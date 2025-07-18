8d675c848572c2f7498dafe2e8b5033607829fbe
Devdot05 <olawoledotun85@gmail.com>
2025-07-18 17:21:36 +0100

    sidebar

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -16,16 +16,13 @@ const Sidebar = ({userId}) => {
 
     return (
         <>
-            <div className={`sidebar d-flex flex-column p-2`}>
-                 <div className='d-block'>
-                <p>welcome, {user.fullName}</p>
-            </div>
+            <div className="position-fixed start-0 bg-white vh-100 d-flex flex-column p-2" style={{width:'200px', top: "60px", zIndex:10, borderRight: "1px solid #ddd"}}>
             <div className=' px-4'>
                 <h3>Menu</h3>
-                <Link to={`/dashboard/${userId}`} className='nav-link'>Dashboard</Link>
-                <Link to={`/allTransaction/${userId}`} className='nav-link'>Transaction</Link>
-                <Link to={`/goal`} className='nav-link'>My Goal</Link>
-                <Link to={`/investment`} className='nav-link'>Investment</Link>
+                <Link to={`/dashboard/${userId}`} className='nav-link d-flex align-item-center mt-3 '>Dashboard</Link>
+                <Link to={`/allTransaction/${userId}`} className='nav-link d-flex align-item-center mt-3 '>Transaction</Link>
+                <Link to={`/goal`} className='nav-link d-flex align-item-center mt-3 '>My Goal</Link>
+                <Link to={`/investment`} className='nav-link d-flex align-item-center mt-3 '>Investment</Link>
             </div>
             </div>
         </>
